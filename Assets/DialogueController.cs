1178c729c9320fb4cca3e74faa0836f131b46858
millandavid <71908868+millandavid@users.noreply.github.com>
2023-01-28 17:35:11 -0600

    Rob's voiceover added
    Added Rob's voice over to the instruction scene

--- Assets/DialogueController.cs
+++ Assets/DialogueController.cs
@@ -7,16 +7,28 @@ using TMPro;
 public class DialogueController : MonoBehaviour
 {
 
+    
+
     public TextMeshProUGUI DialogueText;
     public string[] Sentences;
-    private int Index = 0;
+    private int SentenceIndex = 0;
     public float DialogueSpeed;
     private bool NextText = false;
+
+    public TextMeshProUGUI ContinueText;
+    // public string empty = "";
+    // public string continueText = "Press Space to continue....";
+
+    public AudioSource Source;
+    public AudioClip[] SentencesAudio;
+    private int AudioIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        NextSentence();
+      NextSentence();
+      Source.clip = SentencesAudio[AudioIndex];
+      Source.Play();
     }
 
     // Update is called once per frame
@@ -25,7 +37,10 @@ public class DialogueController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
           if(NextText)
           {
+            ContinueText.text = "";
             NextSentence();
+            Source.clip = SentencesAudio[AudioIndex];
+            Source.Play();
             NextText = false;
           }
         }
@@ -33,7 +48,7 @@ public class DialogueController : MonoBehaviour
 
     void NextSentence()
     {
-      if(Index <= Sentences.Length - 1)
+      if(SentenceIndex <= Sentences.Length - 1)
         {
           DialogueText.text = "";
           StartCoroutine(WriteSentence());
@@ -42,14 +57,17 @@ public class DialogueController : MonoBehaviour
 
     IEnumerator WriteSentence()
     {
-      foreach(char Character in Sentences[Index].ToCharArray())
+      foreach(char Character in Sentences[SentenceIndex].ToCharArray())
       {
         DialogueText.text += Character;
         yield return new WaitForSeconds(DialogueSpeed);
 
       }
-      Index++;
+      SentenceIndex++;
+      AudioIndex++;
       NextText = true;
+      ContinueText.text = "Press Space to continue....";
+
     }
 
 }
